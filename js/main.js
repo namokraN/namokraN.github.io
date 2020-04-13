9adf33a7c0ec6ae3e5f2e5161c9350f4bf32d3cb
namokraN <noname.nsoft@gmail.com>
2020-04-13 12:37:11 +0500

    .

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+// БУРГЕР
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
+	});
+});
+
+// СЛАЙДЕР
+$(document).ready(function() {
+	$('.features__item-title').click(function(event) {
+		$(this).toggleClass('active').next().slideToggle(300);
+		// $('body').toggleClass('lock');
 	});
 });
